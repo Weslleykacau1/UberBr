49df1c8f0423b0b3bdb4be08e68f7872bdd93618
Weslleykacau1 <weslley.kacau@outlook.com>
2025-07-13 14:14:50 -0300

    Fix Map container reinitialization error

--- src/components/dynamic-map.tsx
+++ src/components/dynamic-map.tsx
@@ -46,7 +46,7 @@ export default function MapComponent({ center, drivers, userPos, rideRequest }: 
     const routePositions = rideRequest ? [rideRequest.from.position, rideRequest.to.position] : [];
 
     return (
-        <MapContainer key={JSON.stringify(center)} center={center} zoom={14} scrollWheelZoom={true} style={{ height: '100%', width: '100%' }}>
+        <MapContainer center={center} zoom={14} scrollWheelZoom={true} style={{ height: '100%', width: '100%' }}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
